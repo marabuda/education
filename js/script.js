caf3baf5f42bb37daaeea9a24fa7bdcb04637a7a
yumashen <39693542+yumashen@users.noreply.github.com>
2019-07-15 10:13:48 +0800

    starting another slide show

--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
-
-$(function() {
 	$("#header").load("header.html");
 	$("#footer").load("footer.html");
+$(function() {
     console.log( "ready!" );
 
     $('.slideShow').slick({
@@ -9,8 +8,26 @@ $(function() {
     	// autoplay: true,
 		autoplaySpeed: 7000,
   	});
+    $(document).scroll(function () {
+    	var $nav = $(".fixed-top");
+    	$nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+	});
 
-
+	$('.slider-for').slick({
+	  slidesToShow: 1,
+	  slidesToScroll: 1,
+	  arrows: false,
+	  fade: true,
+	  asNavFor: '.slider-nav'
+	});
+	$('.slider-nav').slick({
+	  slidesToShow: 3,
+	  slidesToScroll: 1,
+	  asNavFor: '.slider-for',
+	  dots: true,
+	  centerMode: true,
+	  focusOnSelect: true
+	});
 
 
 
